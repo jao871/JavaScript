160abc74608f09b12a0737a631d3122c283b0504
João Victor Teodosio Meneghetti <jvt.meneguette@gmail.com>
2026-03-18 08:00:29 -0300

    Add files via upload

---
+++ JavaScript/Backend/OperadoresR.js
@@ -0,0 +1,37 @@
+// exercicio.1 //
+console.log("Maior que: " );  
+console.log("10 > 5:", 10 > 5);       
+console.log("3 > 10:", 3 > 10);       
+console.log("5 > 5:", 5 > 5);    
+
+// exercicio.2 //
+console.log("Menor que: " );  
+console.log("3 < 10:", 3 < 10);       
+console.log("10 < 5:", 10 < 5);       
+console.log("5 < 5:", 5 < 5);         
+
+// exercicio.3 //
+console.log("Maior ou igual que: " );  
+console.log("10 >= 10:", 10 >= 10);   
+console.log("10 >= 5:", 10 >= 5);     
+console.log("3 >= 5:", 3 >= 5);       
+
+//  exercicio.4 //
+console.log("Menor ou igual que: " );
+console.log("5 <= 5:", 5 <= 5);      
+console.log("3 <= 10:", 3 <= 10);     
+console.log("15 <= 10:", 15 <= 10);  
+
+// exercicio.5 //
+console.log("igual á: " );
+console.log("5 == '5':", 5 == '5');  
+console.log("5 == 5:", 5 == 5);      
+console.log("5 == 3:", 5 == 3);       
+
+// exercicio.6 //
+console.log("Diferente de: " );
+console.log("5 != '5': ", 5 != '5');
+console.log("5 != 5:", 5 != 5);
+console.log("5 != 3:", 5 != 3);
+
+
---
+++ JavaScript/README.md
@@ -0,0 +1,4 @@
+# 👋 Hello everyone, I'm a programmer!
+
+I study at ETEC Cidade Tiradentes and I'm looking to improve my programming skills. Here you can see my progress
+in JavaScript. This repository will be especially focused on JavaScript.

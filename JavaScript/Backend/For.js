2af7538a3da0d8c1c653c94801adf7c071bd27df
João Victor Teodosio Meneghetti <jvt.meneguette@gmail.com>
2026-03-18 08:00:48 -0300

    Delete JavaScript directory

--- JavaScript/Backend/For.js
+++
@@ -1,8 +0,0 @@
-
-// exercicio.2 //
-export function tabuada(n1) {
-    for (let i = 0; i <= 10; i++) {
-        const resultado = n1 * i;
-        console.log(i + ' X ' + n1 +' = '+ resultado )
-    }
-}
--- JavaScript/Backend/index.js
+++
@@ -1,4 +0,0 @@
-import { maior, media } from './ArrayVetor'
-import { dobrar } from './map'
-import { verficarIdade, par, reprovado, escolhaDia  } from './IfElse'
-import { tabuada } from './For'

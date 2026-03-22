c3b1f09c15f65d7d8d3cefde0c060c1b0859a1c1
João Victor Teodosio Meneghetti <jvt.meneguette@gmail.com>
2026-03-22 20:40:35 -0300

    Delete Backend directory

--- Backend/map.js
+++
@@ -1,9 +0,0 @@
-// Exercício 1 //
-
-export function dobrar(nums,dobrados) {
-    const nums = [1, 2, 3, 4];
-    const dobrados = nums.map(n => n * 2);
-    console.log('dobrados:', dobrados); 
-    return dobrados;
-}
-dobrar();
